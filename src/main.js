324eecda826413bfe82ff59ca55e500acf0f2948
hubery3721 <hubery3721@gmail.com>
2019-10-24 16:16:31 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Mint from "mint-ui";
+
+Vue.use(Mint);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
